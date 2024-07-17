11bcdebeb6d19c22917c4ae5c789d99aa33b2c88
janelle <jpow1211@gmail.com>
2024-07-17 22:26:22 +0800

    config dialogflow + basic ui

--- frontend/app/screens/Chatbot/index.tsx
+++ frontend/app/screens/Chatbot/index.tsx
@@ -1,9 +1,26 @@
 import { View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+
+const botAvatar = require('../../../assets/images/avatar.png');
+
+const BOT = {
+    _id: 2,
+    name: 'Mr. Bot',
+    avatar: botAvatar,
+}
 
 const Chatbot = () => {
+    const state = {
+        messages: [],
+        id: 1,
+        name: ''
+    }
     return (
-        <View>
-            <Text>Chatbot</Text>
+        <View style={{ marginBottom: 90, flex: 1 }}>
+            <GiftedChat messages={state.messages}
+                onSend={(message) => console.log(message)}
+                onQuickReply={(quickReply) => console.log(quickReply)}
+                user={{ _id: 1 }} />
         </View>
     );
 };
---
+++ frontend/config/dialogflow.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "nuscampuscompass-wri9",
+    "private_key_id": "ffb2967b4b31b73e630f9b2859e268c235ba858b",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCbRxoaSoPuxOms\nQ5Msfa8fe5dSayevO0nvQ9BSpAtApLhvOoCZTbuxUmWlN07N83IQilB8gyLnGBOQ\ndo70PBnlOWoMtodiPrbif9Ef6I0uyeCdOvX34Z7bRF5ANCtqRYpH+kSY8bJBCY+E\nzQlMJpP4a4PuJACo4HhTG05ZAH36Q4iFRTUQRM7bDMWO2OXII+L6CslyuKepewov\nQA9s4r7Lnw1Nqfp/vwPOOM6bySAMO0Un68PqhR7pdMC3CI1mupgdYPVqX3DePdJa\n18j6A74hPDeU0QB64LVY5NKjE5xX2ByiNq0in800CaWmMqws5gKDefJM71iUM+IK\neXLMuGi7AgMBAAECggEAD5Bj7Gi1qrQwG+U2FkqOyCASPd+hva26rnHkqrPosNUR\n64w4oaUqAA8bQyE1E+2n7EKA6MoYMIO4Sk2n7JEto01K9PVCwrU4jaF0K7ealysD\nCZ+EUhFaY+4JaHQSOfe4cZiDUbVAQcacjRokKW7gfBECy1buff7p8KKXCtLCzdfZ\nDyDftx3eeq3ZAgsjZiFazTvlOwnY+7AppaG+5AZFE3zfCwezuZ/VkU5icr6CKr1h\njkC61lHSNTcUXzXwkbjEvJgB5+aTFmCAURxSvMp1ezbqnEqfOq6sLK3WuHWNVL82\n6xKbJow4/b7k8Nc2KtOUPgdfOFX+8ALbQCuG6wjfuQKBgQDQ4CnM6IXgPP6PK4/H\nAjCvhA1vPVMu8bwbV/5Wc1OpXgwaYNsKACM3hLe0/6xxyGYicl/aHMJjGoWpzmhg\nyB3g/gk1DbJogU15giNjSqpyCx+FMxconq7vq44FNi/q7YdMq0LndsIz2FPH/GjS\n+WLRhnn11sTNJW3jJOvIYgGnBQKBgQC+T1WsqqBbDVf/IZ0pyqLYetV3jmclfnPw\nmkB+fQpiP8kFFt4vxkew9ol9BQYAy7PrvEcCWn98/Fzi/PTT5V7FzOPpDR4WeO+e\nWMb0CpJ0+scplfAcAifrCVlR6jWUQN5KNIt6Lb29IoFsWJspNQDUuG0YVlGLPNIa\n/mLzO0tcvwKBgCClWIMpOHtirRqtYUxmYDuedQcU26Jzr8XKS/sxDTtkcpnQ14tJ\nmDd+K8RDTa1+98+cdrS9V/hrVcbDdvbhcCIRMlkzadYBl51NJMDJE7i9WlDUOmNj\n4c2dz5Wq8XRTaZknD07j9dhqkDWqkquexL4guYEd7yZtGi6tP5jTEJUNAoGAPDbd\n5MQX+LdAIHmCQLbcz6IN6MYl3Lcl9/rWbS8jGfMUp4eUxYj3JgbbebMdMyNAylXI\nIfaAYAt5WFixHlp0oiEx+4sq+fFJ39A3IwA1Vs4CNiY60qHb/4H1wPVZ/o6HxvLZ\nLs9KOY2xZZajgWZD3Qp9ejXVlzinQeM7a7pXHgECgYA9gMWAAO/uteVIQl97ig2g\n5yhWKxbrdokcGv5XVtlikbKQjiGkhbQGzUAERFJKYVNSWznxTvQxwxqkmtTIuVqR\n1gHbb6RzJtjeiE6Cp9UpTlRyohyeZwvoKfrJIQ4UfVRbm3RLeylhYuMgbclg5BER\n7XWvhobI8/GWqZuv/5G7yg==\n-----END PRIVATE KEY-----\n",
+    "client_email": "nuscc-chatbot@nuscampuscompass-wri9.iam.gserviceaccount.com",
+    "client_id": "101776043659567044069",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/nuscc-chatbot%40nuscampuscompass-wri9.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
