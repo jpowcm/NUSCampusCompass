3390e6eaac92287ba15f187ca60a8ec9d225a894
janelle <jpow1211@gmail.com>
2024-07-17 22:26:45 +0800

    config dialogflow + basic ui

--- frontend/app/screens/Chatbot/index.tsx
+++ frontend/app/screens/Chatbot/index.tsx
@@ -1,5 +1,7 @@
 import { View, Text } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
+import { dialogflowConfig } from '../../../config/dialogflow';
 
 const botAvatar = require('../../../assets/images/avatar.png');
 
@@ -14,6 +16,13 @@ const Chatbot = () => {
         messages: [],
         id: 1,
         name: ''
+    }
+    const componentDidMount = () => {
+        Dialogflow_V2.setConfiguration(
+            dialogflowConfig.client_email,
+            dialogflowConfig.private_key,
+            Dialogflow_V2.LANG_ENGLISH_US,
+            dialogflowConfig.project_id);
     }
     return (
         <View style={{ marginBottom: 90, flex: 1 }}>
